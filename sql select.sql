7d686e9be712e4c7414c91c35eb69fb0bb083589
AlbandariAltalhi <albandari.altalhi@gmail.com>
2025-03-20 03:59:41 +0300

    Delete sql select.sql

--- sql select.sql
+++
@@ -1,6 +0,0 @@
-SELECT authors.au_id, authors.au_lname, authors.au_fname, COALESCE(SUM(titles.ytd_sales), 0) AS total_sales
-FROM titleauthor
-JOIN authors ON titleauthor.au_id = authors.au_id
-JOIN titles ON titleauthor.title_id = titles.title_id
-GROUP BY authors.au_id
-ORDER BY total_sales DESC;
